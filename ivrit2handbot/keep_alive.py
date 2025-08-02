dcdf19b1ce132c3dd6ad6e9d455094d68adbce64
oleganryb <oleganryb@gmail.com>
2025-08-02 23:35:00 +0300

    Update keep_alive.py

--- ivrit2handbot/keep_alive.py
+++ ivrit2handbot/keep_alive.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return "Бот работает!"
 
-
 def run():
-    app.run(host='0.0.0.0', port=5000, debug=False)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
